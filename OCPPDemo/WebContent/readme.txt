bb8b0f1d3784550e2ad9f749d0b8921935983f95
Patrick Reinhart <patrick@reini.net>
2014-05-29 09:21:49 +0200

    Initial checkin

---
+++ OCPPDemo/WebContent/readme.txt
@@ -0,0 +1,2 @@
+Patricks-MacBook-Air:bin pr$ ./wsdl2java -p ocpp.central http://www.ocppforum.net/sites/default/files/ocpp_centralsystemservice_1.5_final.wsdl
+Patricks-MacBook-Air:bin pr$ ./wsdl2java -p ocpp.chargepoint http://www.ocppforum.net/sites/default/files/ocpp_chargepointservice_1.5_final.wsdl
---
+++ OCPPDemo/src/CentralSystemServiceImpl.java
@@ -0,0 +1,92 @@
+import ocpp.central.AuthorizeRequest;
+import ocpp.central.AuthorizeResponse;
+import ocpp.central.BootNotificationRequest;
+import ocpp.central.BootNotificationResponse;
+import ocpp.central.CentralSystemService;
+import ocpp.central.DataTransferRequest;
+import ocpp.central.DataTransferResponse;
+import ocpp.central.DiagnosticsStatusNotificationRequest;
+import ocpp.central.DiagnosticsStatusNotificationResponse;
+import ocpp.central.FirmwareStatusNotificationRequest;
+import ocpp.central.FirmwareStatusNotificationResponse;
+import ocpp.central.HeartbeatRequest;
+import ocpp.central.HeartbeatResponse;
+import ocpp.central.MeterValuesRequest;
+import ocpp.central.MeterValuesResponse;
+import ocpp.central.StartTransactionRequest;
+import ocpp.central.StartTransactionResponse;
+import ocpp.central.StatusNotificationRequest;
+import ocpp.central.StatusNotificationResponse;
+import ocpp.central.StopTransactionRequest;
+import ocpp.central.StopTransactionResponse;
+
+
+public class CentralSystemServiceImpl implements CentralSystemService {
+
+	@Override
+	public DataTransferResponse dataTransfer(DataTransferRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StatusNotificationResponse statusNotification(
+			StatusNotificationRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StopTransactionResponse stopTransaction(
+			StopTransactionRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BootNotificationResponse bootNotification(
+			BootNotificationRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HeartbeatResponse heartbeat(HeartbeatRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MeterValuesResponse meterValues(MeterValuesRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DiagnosticsStatusNotificationResponse diagnosticsStatusNotification(
+			DiagnosticsStatusNotificationRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AuthorizeResponse authorize(AuthorizeRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FirmwareStatusNotificationResponse firmwareStatusNotification(
+			FirmwareStatusNotificationRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StartTransactionResponse startTransaction(
+			StartTransactionRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
