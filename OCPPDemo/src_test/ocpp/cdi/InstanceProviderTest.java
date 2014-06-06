130043c461471396856a54ef9b803d0813c6180d
Patrick Reinhart <patrick@reini.net>
2014-06-06 22:24:53 +0200

    Enhanced test

--- OCPPDemo/src_test/ocpp/cdi/InstanceProviderTest.java
+++ OCPPDemo/src_test/ocpp/cdi/InstanceProviderTest.java
@@ -2,6 +2,11 @@ package ocpp.cdi;
 
 import static org.junit.Assert.*;
 
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+import javax.xml.datatype.DatatypeFactory;
+
 import org.junit.Test;
 
 /**
@@ -13,12 +18,15 @@ public class InstanceProviderTest {
 
     @Test
     public void testNewDatatypeFactory() {
-        assertNotNull(InstanceProvider.newDatatypeFactory());
+        DatatypeFactory datatypeFactory = InstanceProvider.newDatatypeFactory();
+		assertNotNull(datatypeFactory);
     }
 
     @Test
     public void testNewGregorianCalendar() {
-        assertNotNull(InstanceProvider.newGregorianCalendar());
+        GregorianCalendar calendar = InstanceProvider.newGregorianCalendar();
+		assertNotNull(calendar);
+		assertEquals(TimeZone.getTimeZone("GMT"), calendar.getTimeZone());
     }
 
 }
